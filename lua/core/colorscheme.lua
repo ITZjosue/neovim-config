a86eb1c5880e1699d71c31e401b5f9e8c08d2e43
Josue Lamas <ITZjosue99@gmail.com>
2025-07-18 15:12:53 -0500

    add: colorschemes added

--- lua/core/colorscheme.lua
+++ lua/core/colorscheme.lua
@@ -1,8 +1,14 @@
+-- require('tokyonight').setup({
+--   style = "storm",
+-- })
 -- vim.cmd("colorscheme tokyonight")
 -- vim.cmd("colorscheme catppuccin")
 -- vim.cmd("colorscheme oxocarbon")
 -- vim.cmd("colorscheme nord")
 -- vim.cmd("colorscheme poimandres")
 -- vim.cmd("colorscheme github_dark_high_contrast")
+require("gruvbox").setup({
+  transparent_mode = true,
+})
 vim.cmd("colorscheme gruvbox")
 -- vim.cmd("colorscheme cyberdream")
